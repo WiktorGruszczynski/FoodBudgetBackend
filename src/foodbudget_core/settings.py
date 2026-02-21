a4d91f8bcbf01bb7d8d91e85bfc6836722f22faf
WiktorGruszczynski <wgwiktorwg@gmail.com>
2026-02-21 17:16:59 +0100

    feat: add postgresql support

--- src/foodbudget_core/settings.py
+++ src/foodbudget_core/settings.py
@@ -12,12 +12,16 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import sys, os
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 sys.path.insert(0, str(BASE_DIR / 'src'))
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 
 # Ustawienia
 MEDIA_URL = '/media/'
@@ -27,10 +31,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'src', 'media')
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-op6#8g3sr_xq3^ojmg#s@!$#z9tow^-8hpf^l7*13yyqr(34c-'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-op6#8g3sr_xq3^ojmg#s@!$#z9tow^-8hpf^l7*13yyqr(34c-)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -83,8 +87,12 @@ WSGI_APPLICATION = 'foodbudget_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'), 
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
