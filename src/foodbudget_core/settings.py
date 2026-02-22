2fb3d116701260242c619536e7d2f41bb91a6b44
WiktorGruszczynski <wgwiktorwg@gmail.com>
2026-02-22 23:13:56 +0100

    feat: add product model

--- src/foodbudget_core/settings.py
+++ src/foodbudget_core/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "drf_yasg",
     "drf_spectacular",
     "users",
+    "products",
 ]
 
 REST_FRAMEWORK = {
--- src/products/models.py
+++ src/products/models.py
@@ -1,1 +1,31 @@
-# Create your models here.
+import uuid
+
+from django.conf import settings
+from django.db import models
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128, unique=True)
+    producer = models.CharField(max_length=128)
+
+    issued_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="products")
+
+    # uses either weight or volume
+    weight = models.IntegerField(null=True, blank=True)
+    volume = models.IntegerField(null=True, blank=True)
+
+    # values below are in (grams) per 100g/100ml
+
+    energy = models.FloatField()  # kcal
+
+    fat = models.FloatField()
+    saturated_fat = models.FloatField()
+    carbohydrates = models.FloatField()
+    sugars = models.FloatField()
+    fiber = models.FloatField()
+    protein = models.FloatField()
+    salt = models.FloatField()
+
+    def __str__(self):
+        return f"Product<name={self.name}>"
